e7ed2dc99a664cb05767072fe7b3e74e0841506f
DarkGoanna <darkgoanna@gmail.com>
2021-09-13 16:15:20 +0300

    delete city

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,3 @@
-
 'use strict';
 const html = document.documentElement;
 const header = document.querySelector('.header');
@@ -24,20 +23,27 @@ function addMenuArrow() {
     });
 }
 
-function setIterationVar () {
-    menu.querySelectorAll('.menu > li').forEach((item, i) => {
-        item.style.setProperty('--i', i);
+function setIterationVar() {
+    menu.querySelectorAll('.menu > li').forEach((item, i, arr) => {
+        item.style.setProperty('--in', i);
+        item.style.setProperty('--out', ((arr.length - 1) - i));
+    });
+    menu.querySelectorAll('.sub-menu').forEach(submenu => {
+        submenu.querySelectorAll('li').forEach((item, i, arr) => {
+            item.style.setProperty('--sub_in', i);
+            item.style.setProperty('--sub_out', ((arr.length - 1) - i));
+        });
     });
 }
 
-function addButtonComeback () {
+function addButtonComeback() {
     const comebackButton = '<button type="button" class="menu__comeback">Назад</button>';
     document.querySelectorAll('.sub-menu').forEach(submenu => {
         submenu.insertAdjacentHTML('afterbegin', comebackButton);
     });
 }
 
-function setSubmenuPosition () {
+function setSubmenuPosition() {
     document.querySelectorAll('.sub-menu').forEach(submenu => {
         const menuListTop = menuList.offsetTop;
         const submenuTop = submenu.parentElement.offsetTop;
@@ -46,7 +52,7 @@ function setSubmenuPosition () {
     });
 }
 
-function resetSubmenuPosition () {
+function resetSubmenuPosition() {
     document.querySelectorAll('.sub-menu').forEach(submenu => {
         submenu.style.top = '';
     });
@@ -58,7 +64,7 @@ function addEventToMenuArrow() {
             e.stopPropagation();
             btn.closest('.menu-item-has-children').classList.toggle('open');
             // fixHeaderHeight();
-            if( window.matchMedia('(max-width: 768px)').matches ) {
+            if (window.matchMedia('(max-width: 768px)').matches) {
                 menuList.classList.toggle('show-sub-menu');
             }
         }, true);
@@ -71,7 +77,7 @@ function addEventToMenuItem() {
             e.stopPropagation();
             item.classList.toggle('open');
             // fixHeaderHeight();
-            if( (window.matchMedia('(max-width: 768px)').matches) ) {
+            if ((window.matchMedia('(max-width: 768px)').matches)) {
                 menuList.classList.toggle('show-sub-menu');
             }
         }, true);
@@ -104,7 +110,7 @@ window.addEventListener('load', () => {
     addEventToMenuArrow();
     addEventToMenuItem();
     setIterationVar();
-    if ( window.matchMedia('(max-width: 768px)').matches ) setSubmenuPosition();
+    if (window.matchMedia('(max-width: 768px)').matches) setSubmenuPosition();
 });
 // fixed header
 // фиксим проваливание блока идущего после fixed header
@@ -145,10 +151,100 @@ function isApple() {
         'iPhone',
         'iPod'
     ].includes(navigator.platform)
-    // iPad on iOS 13 detection
-    || (navigator.userAgent.includes('Mac') && 'ontouchend' in document);
+        // iPad on iOS 13 detection
+        || (navigator.userAgent.includes('Mac') && 'ontouchend' in document);
 }
 
 window.addEventListener('load', () => {
     if (isApple()) document.querySelector('html').classList.add('ios');
+});
+
+// склонения
+function declOfNum(n, titles) {
+    return titles[n % 10 == 1 && n % 100 != 11 ? 0 : n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2];
+}
+
+// write number in status field
+function checkboxStatus(value, textOutClassName) {
+    const statusText = document.querySelector(textOutClassName);
+    const correctWord = declOfNum(value, ['элемент', 'элемента', 'элементов']);
+
+    statusText.textContent = `${value} ${correctWord} выбрано`;
+
+    let hasChecked = value ? true : false;
+    statusText.setAttribute('data-hasChecked', hasChecked);
+}
+
+
+// add custom checkbox
+function addCustomCheckbox(parent, checkboxClassName) {
+    const checkboxes = document.querySelectorAll(`${parent} ${checkboxClassName}`);
+    if (checkboxes.length) {
+        let count = 0;
+
+        checkboxes.forEach(checkbox => {
+            checkbox.insertAdjacentHTML('beforeend', '<span class="custom-checkbox"></span>');
+            const input = checkbox.querySelector('input[type="checkbox"]');
+            input.addEventListener('change', function () {
+                count = this.checked ? ++count : --count;
+                checkboxStatus(count, `${parent} .cost-form__status-text`);
+            });
+        });
+        checkboxStatus(count, `${parent} .cost-form__status-text`);
+    }
+}
+addCustomCheckbox('.cost-form__health', '.cost-form__custom-checkbox');
+
+
+// change radio
+function initCustomRadio(parent, radioClassName) {
+    const radios = document.querySelectorAll(`${parent} ${radioClassName}`);
+    if (radios.length) {
+        const output = document.querySelector(`${parent} .cost-form__status-text`);
+        radios.forEach(radio => {
+            const input = radio.querySelector('input[type="radio"]');
+            input.addEventListener('change', function () {
+                if (this.checked) {
+                    output.textContent = input.value;
+                    output.setAttribute('data-hasChecked', true);
+                }
+            });
+        });
+    }
+}
+initCustomRadio('.cost-form__city', '.cost-form__custom-checkbox');
+
+
+// open/close checkbox menu
+function openCustomSelect(parent) {
+    const checkboxMenu = document.querySelector(`${parent} .cost-form__checkbox-wrapper`);
+    const checkboxArrow = document.querySelector(`${parent} .cost-form__arrow`);
+    document.addEventListener('click', event => {
+        const { target } = event;
+
+        if (target.classList.contains(`${parent} cost-form__checkbox-wrapper`)
+            || target.closest(`${parent} .cost-form__checkbox-wrapper`)) {
+            checkboxMenu.classList.add('open');
+            checkboxArrow.classList.add('open');
+        } else if (target.classList.contains(`${parent} cost-form__status`)
+            || target.closest(`${parent} .cost-form__status`)) {
+            checkboxMenu.classList.toggle('open');
+            checkboxArrow.classList.toggle('open');
+        } else {
+            checkboxMenu.classList.remove('open');
+            checkboxArrow.classList.remove('open');
+        }
+    });
+}
+
+// состояние зоровья
+openCustomSelect('.cost-form__health');
+// города
+openCustomSelect('.cost-form__city');
+
+// cost-form trigger
+document.querySelector('.cost-form__trigger').addEventListener('click', function () {
+    const form = this.closest('.cost-form');
+    form.classList.toggle('open');
+    this.classList.toggle('hidden');
 });
